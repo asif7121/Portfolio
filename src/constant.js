a6fadfab0ae57b298cac086b516b1f2d9e6964d5
Asif <ansariasif449@gmail.com>
2024-02-01 17:57:00 +0530

    first commit

---
+++ src/constant.js
@@ -0,0 +1,6 @@
+export default {
+  emailto: "mailto:ansariasif449@gmail.com",
+  email: "ansariasif449@gmail.com",
+  linkedin: "www.linkedin.com/in/asif7121",
+  github: "https://github.com/asif7121",
+};
