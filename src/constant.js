b77b0fd7e3624e8cdaa7aa8eb58bd5499c03d412
Asif <ansariasif449@gmail.com>
2024-02-05 12:02:34 +0530

    fixed linkedin profile url

--- src/constant.js
+++ src/constant.js
@@ -1,6 +1,10 @@
-export default {
-  emailto: "mailto:ansariasif449@gmail.com",
-  email: "ansariasif449@gmail.com",
-  linkedin: "www.linkedin.com/in/asif7121",
-  github: "https://github.com/asif7121",
-};
+
+
+ export default {
+   emailto: "mailto:ansariasif449@gmail.com",
+   email: "ansariasif449@gmail.com",
+   linkedin: "https://linkedin.com/in/asif7121",
+   github: "https://github.com/asif7121",
+ };
+
+
